8b3de9df8906c27a44fa1aafb184988cc5a230bf
Elliot <elliotmason64@gmail.com>
2025-02-18 15:46:23 +1100

    Compentized application
    Add StationHeader and TrainList components, implement useFetchTrains hook, and introduce train icon

--- src/TrainDepartures.js
+++ src/TrainDepartures.js
@@ -1,42 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import trainIcon from './trainIcon.png';
+import React, { useState } from 'react';
 import './TrainDepartures.css'; // Import the CSS file
+import StationHeader from './Components/StationHeader/StationHeader';
+import TrainList from './Components/TrainList/TrainList';
+import useFetchTrains from './useFetchTrains';
 
 const TrainDepartures = () => {
-  const [trains, setTrains] = useState([]);
-  const [error, setError] = useState(null);
   const [nameOrigin, setNameOrigin] = useState('10101252'); // Penrith Station
   const [nameDestination, setNameDestination] = useState('10101100'); // Central Station
-
-  const fetchTrains = async (origin, destination) => {
-    try {
-      const response = await axios.get('http://localhost:5000/api/trains', {
-        params: {
-          name_origin: origin,
-          name_destination: destination,
-        },
-      });
-      const data = response.data;
-      console.log('Train Data:', data);
-
-      // Access the first 5 train departures
-      const trainDepartures = data.slice(0, 5);
-      setTrains(trainDepartures);
-    } catch (error) {
-      setError(error.message);
-    }
-  };
-
-  useEffect(() => {
-    fetchTrains(nameOrigin, nameDestination);
-
-    const interval = setInterval(() => {
-      fetchTrains(nameOrigin, nameDestination);
-    }, 60000); // 60000 milliseconds = 1 minute
-
-    return () => clearInterval(interval); // Clear interval on component unmount
-  }, [nameOrigin, nameDestination]);
+  const { trains, error } = useFetchTrains(nameOrigin, nameDestination);
 
   const swapStations = () => {
     setNameOrigin(nameDestination);
@@ -59,41 +30,14 @@ const TrainDepartures = () => {
 
   return (
     <div>
-      <nav className="navbar">
-        <div className="station-container">
-          <h1 className="station-name">
-            {trains.length > 0 && getStationAndPlatform(trains[0].legs[0].origin.name).station}
-          </h1>
-          <button onClick={swapStations} className="swap-button">
-            <img src={trainIcon} alt="Swap Origin and Destination" className="swap-icon" />
-          </button>
-          <h1 className="station-name">
-            {trains.length > 0 && getStationAndPlatform(trains[0].legs[0].destination.name).station}
-          </h1>
-        </div>
-      </nav>
-      <div style={
-        { marginTop: '40px' }
-      }></div>
+      <StationHeader
+        origin={trains.length > 0 ? getStationAndPlatform(trains[0].legs[0].origin.name).station : ''}
+        destination={trains.length > 0 ? getStationAndPlatform(trains[0].legs[0].destination.name).station : ''}
+        swapStations={swapStations}
+      />
+      <div style={{ marginTop: '40px' }}></div>
       {error && <p>Error: {error}</p>}
-      <ul>
-        {trains.map((train, index) => (
-          <React.Fragment key={index}>
-            <li>
-              <div>
-                <strong>Departure Time:</strong> {formatDateTime(train.legs[0].origin.departureTimeEstimated)}  <strong>Departs from:</strong> {getStationAndPlatform(train.legs[0].origin.name).platform}
-              </div>
-              <div>
-                <strong>Arrival Time:</strong> {formatDateTime(train.legs[0].destination.arrivalTimeEstimated)}  <strong>Arrives to:</strong> {getStationAndPlatform(train.legs[0].destination.name).platform}
-              </div>
-              <div>
-                <strong>Train Type:</strong> {train.legs[0].transportation.disassembledName}
-              </div>
-            </li>
-            {index < trains.length - 1 && <hr className="solid" />}
-          </React.Fragment>
-        ))}
-      </ul>
+      <TrainList trains={trains} formatDateTime={formatDateTime} getStationAndPlatform={getStationAndPlatform} />
     </div>
   );
 };
