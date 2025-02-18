7e8ac8d475d671ed03376ddc7da7befc13163199
Elliot <elliotmason64@gmail.com>
2025-02-18 16:01:04 +1100

    Refactor TrainItem component to simplify display and rename 'Train Type' to 'Route Type'

--- src/Components/TrainItem/TrainItem.js
+++ src/Components/TrainItem/TrainItem.js
@@ -4,19 +4,13 @@ const TrainItem = ({ train, formatDateTime, getStationAndPlatform }) => {
   return (
     <li>
       <div>
-        <strong>Origin:</strong> {getStationAndPlatform(train.legs[0].origin.name).station}
-      </div>
-      <div>
         <strong>Departure Time:</strong> {formatDateTime(train.legs[0].origin.departureTimeEstimated)} - <strong>Platform:</strong> {getStationAndPlatform(train.legs[0].origin.name).platform}
-      </div>
-      <div>
-        <strong>Destination:</strong> {getStationAndPlatform(train.legs[0].destination.name).station}
       </div>
       <div>
         <strong>Arrival Time:</strong> {formatDateTime(train.legs[0].destination.arrivalTimeEstimated)} - <strong>Platform:</strong> {getStationAndPlatform(train.legs[0].destination.name).platform}
       </div>
       <div>
-        <strong>Train Type:</strong> {train.legs[0].transportation.disassembledName}
+        <strong>Route Type:</strong> {train.legs[0].transportation.disassembledName}
       </div>
     </li>
   );
