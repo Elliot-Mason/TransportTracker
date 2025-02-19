0333a6d271a3995163f2986ddecd8fe94e63873f
Elliot <elliotmason64@gmail.com>
2025-02-19 14:50:23 +1100

    Add StationHeader component with styling; refactor CSS for better layout and separation

--- src/Components/StationHeader/StationHeader.js
+++ src/Components/StationHeader/StationHeader.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import trainIcon from '../../Assets/trainIcon.png';
+import './StationHeader.css';
 
 const StationHeader = ({ origin, destination, swapStations }) => {
   return (
     <nav className="navbar">
       <div className="station-container">
-        <h1 className="station-name">{origin}</h1>
+        <div className="station-name-container">
+          <h1 className="station-name">{origin}</h1>
+        </div>
         <button onClick={swapStations} className="swap-button">
           <img src={trainIcon} alt="Swap Origin and Destination" className="swap-icon" />
         </button>
-        <h1 className="station-name">{destination}</h1>
+        <div className="station-name-container">
+          <h1 className="station-name">{destination}</h1>
+        </div>
       </div>
     </nav>
   );
