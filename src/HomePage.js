fcb5655564e1d06273b5c797900d83f6ae49a8b7
Elliot <elliotmason64@gmail.com>
2025-02-23 13:44:25 +1100

    Add search functionality toggle in HomePage component

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import SearchForm from './Components/SearchForm/SearchForm';
 import { Button } from '@material-ui/core';
 
 const HomePage = () => {
   const navigate = useNavigate();
+  const [showSearchForm, setShowSearchForm] = useState(false);
 
   const handleSearch = (originId, destinationId) => {
     navigate(`/departures?origin=${originId}&destination=${destinationId}`);
+  };
+
+  const handleTrainClick = () => {
+    setShowSearchForm(true);
   };
 
   return (
     <div>
       <h1>Welcome to the Train Departures App</h1>
       <div>
-        {/* <Button text='General search '> </Button>
-        <Button> Train </Button>
+      <button onClick={handleSearch}>General Search</button>
+      <button onClick={handleTrainClick}> Train </button>
+        {/*
+        implement these functions later
+        
         <Button> Bus </Button>
         <Button> LightRail </Button>
         <Button> Ferry </Button>
         <Button> Metro </Button> */}
       </div>
-      <SearchForm onSearch={handleSearch} />
+      {showSearchForm && <SearchForm onSearch={handleSearch} />}
     </div>
   );
 };
