a0d413f7ec4ea9b60a47a6a55fbb8ba1bca7e854
Elliot <elliotmason64@gmail.com>
2025-03-01 17:26:51 +1100

    Add MetroDepartures route to the application

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './Pages/HomePage/HomePage';
 import TrainDepartures from './Pages/TrainDepartures/TrainDepartures';
+import MetroDepartures from './Pages/MetroDepartures/MetroDepartures';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<HomePage />} />
           <Route path="/TrainDepartures" element={<TrainDepartures />} />
+          <Route path="/MetroDepartures" element={<MetroDepartures />} />
         </Routes>
       </div>
     </Router>
